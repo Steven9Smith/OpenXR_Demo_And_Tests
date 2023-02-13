ca5404d361af780eea2f44c504c513285c6b9291
Steven9Smith <steven9smith9@gmail.com>
2023-02-13 16:27:49 -0500

    a

--- Assets/Scenes/VR_Class/StarterAssets/FirstPersonController/Scripts/BasicRigidBodyPush.cs
+++ Assets/Scenes/VR_Class/StarterAssets/FirstPersonController/Scripts/BasicRigidBodyPush.cs
@@ -4,7 +4,7 @@ public class BasicRigidBodyPush : MonoBehaviour
 {
 	public LayerMask pushLayers;
 	public bool canPush;
-	[Range(0.5f, 5f)] public float strength = 1.1f;
+	[Range(0.5f, 500f)] public float strength = 1.1f;
 
 	private void OnControllerColliderHit(ControllerColliderHit hit)
 	{
@@ -31,5 +31,8 @@ public class BasicRigidBodyPush : MonoBehaviour
 
 		// Apply the push and take strength into account
 		body.AddForce(pushDir * strength, ForceMode.Impulse);
+
+	
+
 	}
 }
